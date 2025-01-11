8a4b139b9759c0971022a3320376c3e1c1bb4251
subhanali <117721076+Subhanali000@users.noreply.github.com>
2025-01-11 13:58:20 +0530

    Update openAI.py

--- openAI.py
+++ openAI.py
@@ -2,7 +2,7 @@
 import openai
 
 # Replace with your actual API key
-openai.api_key = "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"
+openai.api_key = "Enter here Your API key"
 
 response = openai.Completion.create(
     model="gpt-3.5-turbo",
